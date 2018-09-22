7003fc3db840030bdb637491367dd85cc7b90de3
Michael Jonathan Lee <code.mjlee@gmail.com>
2018-09-22 22:50:11 +0200

    Allow users to use the full email app.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-module.exports = Franz => Franz
+module.exports = Franz => class Messenger extends Franz {
+    overrideUserAgent() {
+        // yes we really need to do this
+        // otherwise we are stuck with the light version of the email app
+        return 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+    }
+}
