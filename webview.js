9932998a11f26a14db27de2b97fd82fdc29e8822
Michael Jonathan Lee <code.mjlee@gmail.com>
2018-09-23 00:43:22 +0200

    Make the unread messages display work for light and full version of web app..

--- webview.js
+++ webview.js
@@ -1,22 +1,55 @@
 module.exports = Franz => {
-  const labelNumberExpr = /\(([0-9]+)\)/
+  function setNumberOfUnreadMails(numberText) {
+    const count = parseInt(numberText, 10)
+    if (isNaN(count) || count < 0) {
+      throw 'error parsing number of mails'
+    }
+    Franz.setBadge(count)
+  }
+  
+  function trySetNumberOfUnreadMailsForLightApp() {
+    numberLabels = document.getElementsByClassName('unrd')
+    
+    if (numberLabels.length === 0) {
+      return false
+    }
+
+    if (numberLabels.length < 2) {
+      throw 'bad number of unread labels'
+    }
+    const label = numberLabels[1]
+
+    const labelNumberExpr = /\(([0-9]+)\)/
+    const matchResult = labelNumberExpr.exec(label.innerText)
+    if (!matchResult || matchResult.length < 2) {
+      throw 'unexpected label content'
+    }
+
+    setNumberOfUnreadMails(matchResult[1])
+    return true
+  }
+
+  function trySetNumberOfUnreadMailsForFullApp() {
+    numberLabel = document.getElementById('spnCV')
+    if (!numberLabel) {
+      return false
+    }
+    
+    setNumberOfUnreadMails(numberLabel.innerText)
+    return true
+  }
 
   Franz.loop(() => {
     try {
-      const unread = document.getElementsByClassName('unrd')
-      if (!unread || unread.length < 2) {
-        throw 'can not find unread label in document'
+      if (trySetNumberOfUnreadMailsForLightApp()) {
+        return
+      } 
+      
+      if (trySetNumberOfUnreadMailsForFullApp()) {
+        return
       }
-      const label = unread[1]
-      const matchResult = labelNumberExpr.exec(label.innerText)
-      if (!matchResult || matchResult.length < 2) {
-        throw 'unexpected label content'
-      }
-      const count = parseInt(matchResult[1], 10)
-      if (isNaN(count) || count < 0) {
-        throw 'error parsing number of mails'
-      }
-      Franz.setBadge(count)
+
+      throw 'unknown app version'
     } catch (e) {
       console.warn('failed to determine number of unread messages:')
       console.warn(e)
